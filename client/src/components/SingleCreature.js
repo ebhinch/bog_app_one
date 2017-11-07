2574b1afa35ffb00b2383cfdfb73f00d29b34ef0
ebhinch <erica.hinchman@gmail.com>
2017-11-07 16:00:45 -0500

    got changes to creature to save

--- client/src/components/SingleCreature.js
+++ client/src/components/SingleCreature.js
@@ -11,27 +11,39 @@ class SingleCreature extends Component {
         showUpdateCreature: false
     }
 
-    componentWillMount () {
+    componentWillMount() {
         this.getCreature()
     }
 
     getCreature = async () => {
         const creatureId = this.props.match.params.id
         const response = await axios.get(`/api/creatures/${creatureId}`)
-        this.setState({creature: response.data})
+        this.setState({ creature: response.data })
     }
 
     toggleUpdateCreatureInfo = () => {
-        this.setState({showUpdateCreature: !this.state.showUpdateCreature})
+        this.setState({ showUpdateCreature: !this.state.showUpdateCreature })
     }
 
     handleChange = (event) => {
         try {
-            const clonedCreature = {...this.state.creature }          
+            const clonedCreature = { ...this.state.creature }
             const attribute = event.target.name
             clonedCreature[attribute] = event.target.value
 
-            this.setState({creature: clonedCreature})
+            this.setState({ creature: clonedCreature })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    handleSubmit = async (event) => {
+        try {
+            event.preventDefault()
+            const creatureId = this.props.match.params.id
+            const updatedCreature = {...this.state.creature}
+            const response = await axios.patch(`/api/creatures/${creatureId}`, {creature: updatedCreature})
 
         } catch (error) {
             console.log(error)
@@ -39,29 +51,19 @@ class SingleCreature extends Component {
     }
 
 
-    handleSubmit = async (event) => {
-        event.preventDefault()
-        const creatureId = this.props.id
-        const updatedState = {...this.state.creatureId}
-        const response = await axios.patch(`/creatures/${creatureId}`, {
-            creature: updatedState })
-            this.props.toggleEdit()
-            this.props.getPosts()
+    render() {
+        return (
+            <div>
+
+                <h1>{this.state.creature.name}</h1>
+                <h3>{this.state.creature.description}</h3>
+                <button onClick={this.toggleUpdateCreatureInfo}>Update Creature Details</button>
+
+                {this.state.showUpdateCreature ? <UpdateCreature name={this.state.creature.name} description={this.state.creature.description} handleChange={this.handleChange} handleSubmit={this.handleSubmit} toggleUpdateCreatureInfo={this.toggleUpdateCreatureInfo} /> : null}
+
+            </div>
+        );
     }
-
-  render() {
-    return (
-      <div>
-       
-        <h1>{this.state.creature.name}</h1>
-        <h3>{this.state.creature.description}</h3>
-        <button onClick={this.toggleUpdateCreatureInfo}>Update Creature Details</button>
-
-        {this.state.showUpdateCreature ? <UpdateCreature name={this.state.creature.name} description={this.state.creature.description} handleChange={this.handleChange} handleSubmit={this.handleSubmit} toggleUpdateCreatureInfo={this.toggleUpdateCreatureInfo} /> : null}
-       
-      </div>
-    );
-  }
 }
 
 export default SingleCreature;
